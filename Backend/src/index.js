b06be6c3deae210194001933f6475a7310cb6647
Ganesh S <133591305+Ganesh6811@users.noreply.github.com>
2025-04-23 15:36:01 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "https://subscription-tracker-frontend.onrender.com", 
     credentials: true,  
     methods: ["GET", "POST", "PUT", "DELETE"],   
     allowedHeaders: ["Content-Type", "Authorization"],   
