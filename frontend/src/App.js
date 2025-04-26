f1677468a0dd2d5abfb2fba50f12d532a652950f
Ganesh S <133591305+Ganesh6811@users.noreply.github.com>
2025-04-26 10:48:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,42 @@
-import { BrowserRouter, Routes, Route, Navigate , HashRouter} from "react-router-dom"; 
-import {HomePage} from './Pages/Home/HomePage.jsx';
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HomePage } from './Pages/Home/HomePage.jsx';
 import LoginPage from './Pages/Login/LoginPage.jsx';
 import SignUpPage from "./Pages/Registration/SignUpPage.jsx";
 import ProfilePage from "./Pages/Profile/ProfilePage.jsx";
 
-
 import React, { useState, useEffect, createContext } from 'react';
-import {AuthUser} from './Services/Auth.jsx';
+import { AuthUser } from './Services/Auth.jsx';
 
 export const store = createContext();
 
 function App() {
-
   const [isAuthenticated, setIsAuthenticated] = useState(null);
   const [user, setUser] = useState(null);
 
-  useEffect(()=>{
-    const checkAuthStatus = async()=>{
+  useEffect(() => {
+    const checkAuthStatus = async () => {
       const checkUser = await AuthUser();
       setIsAuthenticated(checkUser.check);
       setUser(checkUser.userId);
-    }
+    };
 
     checkAuthStatus();
-  }, [isAuthenticated]);
+  }, []);
 
-  if(isAuthenticated == null){
-    return <div>Loading......</div>
+  if (isAuthenticated == null) {
+    return <div>Loading......</div>;
   }
 
-  
   return (
-    <store.Provider value ={[user, setUser]}>
-      <BrowserRouter>
+    <store.Provider value={[user, setUser]}>
       <HashRouter>
         <Routes>
-          <Route path ="/" element={isAuthenticated ? <HomePage /> : <Navigate to = "/login" />} />
-          <Route path = "/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to = "/" />} />
-          <Route path = "/signUp" element={!isAuthenticated ? <SignUpPage /> : <Navigate to = "/" />} />
-          <Route path = "/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate to = "/login" />} />
+          <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/signUp" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+          <Route path="/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />} />
         </Routes>
-       </HashRouter>
-      </BrowserRouter>
+      </HashRouter>
     </store.Provider>
   );
 }
